6159fde76216971e6742eb22b8ba87a327450836
AurelTchn <aureltchanhouin@gmail.com>
2024-08-05 17:48:50 +0100

    Update

--- db.js
+++ db.js
@@ -5,6 +5,9 @@ const pool = mysql.createPool({
     user: 'if0_36920580',
     password: 'nN1jPe7lLKAq0Be',
     database: 'if0_36920580_elearning',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
     port: '3306'
     
 })
